024444437a3401e5aaa77caa54ac14d91984d2b2
Marta Menac <martamenacayats@gmail.com>
2024-05-05 09:55:28 +0200

    Created initial Tile and Grid classes

---
+++ Assets/Scripts/Core/Grid.cs
@@ -0,0 +1,30 @@
+﻿namespace MMA.Core
+{
+    public class Grid
+    {
+        public int NumberOfRows;
+        public int NumberOfColumns;
+
+        public Tile[][] Tiles;
+
+        public void GenerateTiles()
+        {
+
+        }
+
+        public void SwapTiles()
+        {
+
+        }
+
+        public void ShiftColumnDown()
+        {
+
+        }
+
+        public void SpawnNewTile()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Tile.cs
@@ -0,0 +1,7 @@
+namespace MMA.Core
+{
+    public class Tile
+    {
+        public int TileType;
+    }
+}
