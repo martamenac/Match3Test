02a55e8f6fcc468af30eeda2e0d12a0f07bf01d4
Marta Menac <martamenacayats@gmail.com>
2024-05-05 10:14:18 +0200

    Added basic UI prefabs & initial scripts

---
+++ Assets/Scripts/UI/GridUIHandler.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using Grid = MMA.Core.Grid;
+namespace MMA.UI
+{
+    public class GridUIHandler : MonoBehaviour
+    {
+        public Grid Grid;
+    }
+}
---
+++ Assets/Scripts/UI/TileUIHandler.cs
@@ -0,0 +1,10 @@
+﻿using MMA.Core;
+using UnityEngine;
+
+namespace MMA.UI
+{
+    public class TileUIHandler : MonoBehaviour
+    {
+        public Tile Tile;
+    }
+}
